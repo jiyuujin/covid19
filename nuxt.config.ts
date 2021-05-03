b9f13f15bc327c1b5178339eefaa75554c5a1f9e
jiyuujin.tsx <jiyuujinunite@gmail.com>
2021-05-03 22:11:07 +0900

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+export default {
+  head: {
+    title: 'top',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  css: [],
+
+  components: true,
+
+  buildModules: ['@nuxt/typescript-build'],
+
+  modules: ['@nuxtjs/pwa'],
+
+  build: {}
+}
